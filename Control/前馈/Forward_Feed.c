7789892348f45fc95868689d4eb6c3f69b4f919e
Tacrine <tacrine_f@qq.com>
2026-02-12 00:49:43 +0800

    feat(Control): 添加前馈控制算法实现
    新增前馈控制算法的初始化和计算函数。
    - 在Forward_Feed.h中定义了Feed_Forward_t结构体，包含了输入值、输出值、经验因子和目标值。
    - 在Forward_Feed.c中实现了Forward_Feed_Init函数，用于初始化前馈控制参数。
    - 在Forward_Feed.c中实现了Forward_Feed_Calculate函数，用于根据输入值计算输出值，采用误差平方与经验因子和目标值的比值作为输出。
    - 在Forward_Feed.c中实现了Forward_Feed_All_Init函数，用于全局初始化前馈控制参数。

---
+++ Control/前馈/Forward_Feed.c
@@ -0,0 +1,23 @@
+#include "Forward_Feed.h"
+
+Feed_Forward_t feed_forward;
+
+void Forward_Feed_Init(Feed_Forward_t *feed_forward, float K, float target)
+{
+    feed_forward->input = 0;
+    feed_forward->output = 0;
+
+    feed_forward->K = K;
+    feed_forward->target = target;
+}
+
+float Forward_Feed_Calculate(Feed_Forward_t *feed_forward, float input)
+{
+    float error = feed_forward->target - feed_forward->input;
+    feed_forward->output = feed_forward->K * error * error * 1 / feed_forward->target;     // 输出值 = 经验因子 * 误差^`2 * 1/目标值
+}
+
+void Forward_Feed_All_Init(void)
+{
+    Forward_Feed_Init(&feed_forward, 0.01, 100);
+}
---
+++ Control/前馈/Forward_Feed.h
@@ -0,0 +1,20 @@
+#ifndef __Feed_Forward_H
+#define __Feed_Forward_H
+
+typedef struct {
+    float input;
+
+    float K;    //经验因子，需要手动调整
+    float target;
+
+    float output;
+} Feed_Forward_t;
+extern Feed_Forward_t feed_forward;
+
+
+void Forward_Feed_Init(Feed_Forward_t *feed_forward, float K, float target);
+
+float Forward_Feed_Calculate(Feed_Forward_t *feed_forward, float input);
+
+void Forward_Feed_All_Init(void);
+#endif // __Feed_Forward_H 
