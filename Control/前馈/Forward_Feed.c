b708f0be95240933eb92ffcdfda9d5343387d66b
Tacrine <tacrine_f@qq.com>
2026-02-12 00:56:30 +0800

    fix(Control): 修正前馈控制输出计算公式
    修正前馈控制输出计算公式，更新为三次误差乘积而不是二次误差乘积。具体修改如下：
    
    - 将输出计算公式从 `K * error^2 * 1/target` 修改为 `- (K * error^3 * 1/target)`。此更改旨在提高控制算法的响应精度和稳定性。

--- Control/前馈/Forward_Feed.c
+++ Control/前馈/Forward_Feed.c
@@ -14,7 +14,7 @@ void Forward_Feed_Init(Feed_Forward_t *feed_forward, float K, float target)
 float Forward_Feed_Calculate(Feed_Forward_t *feed_forward, float input)
 {
     float error = feed_forward->target - feed_forward->input;
-    feed_forward->output = feed_forward->K * error * error * 1 / feed_forward->target;     // 输出值 = 经验因子 * 误差^`2 * 1/目标值
+    feed_forward->output = - ((feed_forward->K) * error * error * error * (1 / feed_forward->target));     // 输出值 = 经验因子 * 误差^`3 * 1/目标值
 }
 
 void Forward_Feed_All_Init(void)
