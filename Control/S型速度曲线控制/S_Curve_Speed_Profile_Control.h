0e934e60f44816d9eed37f53f0034e739c4dedbe
Tacrine <tacrine_f@qq.com>
2026-03-16 22:04:04 +0800

    feat: S型速度曲线控制
    修正S型速度曲线控制初始化函数参数，确保使用正确的控制器实例。
    在头文件中添加S型速度曲线控制相关函数的声明，提高代码模块化和可维护性。

--- Control/S型速度曲线控制/S_Curve_Speed_Profile_Control.h
+++ Control/S型速度曲线控制/S_Curve_Speed_Profile_Control.h
@@ -15,4 +15,8 @@ typedef struct {
     float step;      // 输出值，该值实际意义是速度，该算法的输出值应该直接累加到PWM占空比之类的输出上，是积分控制
 } Tac_S_Curve_Controller;
 extern Tac_S_Curve_Controller S_Curve_Controller;
+
+void S_Curve_Controller_Init(Tac_S_Curve_Controller *S_Curve_Controller);
+float S_Curve_Controller_Update(Tac_S_Curve_Controller *S_Curve_Controller);
+void S_Curve_Controller_ALL_Init(void);
 #endif //__S_CURVE_SPEED_PROFILE_CONTROL_H
