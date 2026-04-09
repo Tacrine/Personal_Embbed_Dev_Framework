e4c6074e5e7de350b4a1050cc7fe2722135261b0
Tacrine <tacrine_f@qq.com>
2026-04-09 17:50:56 +0800

    fix: 修复S型速度曲线控制器的外部变量声明为volatile，确保数据一致性；调整前馈计算逻辑，修正输出值符号

--- Control/前馈/Forward_Feed.c
+++ Control/前馈/Forward_Feed.c
@@ -11,11 +11,20 @@ void Forward_Feed_Init(Feed_Forward_t *feed_forward, float K, float target)
     feed_forward->target = target;
 }
 
+// 非线性前馈，需要注意当误差较大时输出值会迅速增大，可能会导致系统不稳定，因此需要根据实际情况调整经验因子K和目标值target
 float Forward_Feed_Calculate(Feed_Forward_t *feed_forward, float input)
 {
     float error = feed_forward->target - feed_forward->input;
-    feed_forward->output = - ((feed_forward->K) * error * error * error * (1 / feed_forward->target));     // 输出值 = 经验因子 * 误差^`3 * 1/目标值
+    feed_forward->output =  ((feed_forward->K) * error * error * error * (1 / feed_forward->target));     // 输出值 = 经验因子 * 误差^`3 * 1/目标值
 }
+
+/*  线性前馈
+float Forward_Feed_Calculate(Feed_Forward_t *feed_forward, float input)
+{
+    float error = feed_forward->target - feed_forward->input;
+    feed_forward->output =  ((feed_forward->K) * error * (1 / feed_forward->target));     // 输出值 = 经验因子 * 误差 * 1/目标值
+}
+*/
 
 void Forward_Feed_All_Init(void)
 {
